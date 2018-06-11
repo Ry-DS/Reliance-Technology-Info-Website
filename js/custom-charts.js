6049a3fd9cbdc1da4410bb2bc59dca38eea8f206
SimplyBallistic <ryan2002.rs@gmail.com>
2018-06-11 23:59:21 +1000

    Added Graphs working

--- js/custom-charts.js
+++ js/custom-charts.js
@@ -34,19 +34,69 @@ $( document ).ready(function() {
 var ctx = document.getElementById('careers').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'pie',
+	responsive: true,
+	maintainAspectRatio: false,
 
     // The data for our dataset
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-            label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
+      labels: ["Army", "Arts", "Customer Service", "Finance", "Medicine","Science","Entertainer"],
+      datasets: [{
+        label: "Number of students who selected",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#c45822","#DAe657"],
+        data: [1,2,1,2,4,8,1]
+      }]
     },
+    options: {
+      title: {
+        display: true,
+        text: 'Future Careers students plan to take'
+      }
+    }
+});
+ctx=document.getElementById('graph').getContext('2d');
+chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+	responsive: true,
+	maintainAspectRatio: false,
 
-    // Configuration options go here
-    options: {}
+    // The data for our dataset
+    data: {
+      labels: ["Yes", "No"],
+      datasets: [{
+        label: "Amount",
+        backgroundColor: ["#3e95cd", "#8e5ea2"],
+        data: [10,9]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Number of students who believe robots will have a negative impact'
+      }
+    }
+});
+ctx=document.getElementById('line').getContext('2d');
+chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'radar',
+	responsive: true,
+	maintainAspectRatio: false,
+
+    // The data for our dataset
+    data: {
+      labels: ["1", "2","3","4","5"],
+      datasets: [{
+        label: "Amount",
+        backgroundColor: ["#3e34cd"],
+        data: [5,0,5,4,5]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Comfort Rating of students working alongside robots'
+      }
+    }
 });
